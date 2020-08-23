17e0574ce0e8d0181c696cb9c16acdc3b9331959
Haritham <33144819+Harithahhh@users.noreply.github.com>
2020-08-23 08:12:53 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{ 
+    std::string charactername = "Rachel" ;
+    int characterage  ;
+    characterage = 30 ;
+    std::cout <<"hello my name is " <<charactername <<"\n"  ;
+    std::cout <<"I am "<< characterage <<"years old" <<"\n" ;
+    return 0;
+}
